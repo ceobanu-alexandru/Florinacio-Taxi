818c2c8aa05895c1d14fe631305848e16b3e1501
Alexandra <ceobanu_alexandra@yahoo.com>
2026-02-21 19:11:25 +0200

    backend connection with postgresql database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
-
+const pool = require("./database/db");
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend running" });
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
 app.listen(3000, () => {
